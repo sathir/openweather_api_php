debbbe62eb73e2ebb300da998f0ccc2c665df252
Mansoor M. Sathir <sathir@live.com>
2022-04-18 06:02:14 +0530

    Interfaces

---
+++ index.php
@@ -0,0 +1,111 @@
+<?php
+require_once('request.php');
+
+class weatherAPI{
+
+    function getWeatherReport($city){
+        $api_key = "API KEY HERE";
+        $url = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$api_key";
+        $response = SimpleJsonRequest::get($url);
+
+        return json_encode($response, JSON_PRETTY_PRINT);
+    }
+}
+
+    $weatherAPI = new weatherAPI();
+
+    if (isset($_GET['city'])){
+
+    try {
+        $response = $weatherAPI->getWeatherReport($_GET['city']);
+        $data = json_decode($response);
+        $currentTime = time();
+    }
+    catch(Exception $e) {
+        echo 'Message: ' .$e->getMessage();
+    }
+?>
+<!doctype html>
+<html>
+<head>
+    <title>Weather API</title>
+
+    <style>
+        body {
+            font-family: Arial;
+            font-size: 0.95em;
+            color: #929292;
+        }
+
+        .report-container {
+            border: #E0E0E0 1px solid;
+            padding: 20px 40px 40px 40px;
+            border-radius: 2px;
+            width: 550px;
+            margin: 0 auto;
+        }
+
+        .weather-icon {
+            vertical-align: middle;
+            margin-right: 20px;
+        }
+
+        .weather-forecast {
+            color: #212121;
+            font-size: 1.2em;
+            font-weight: bold;
+            margin: 20px 0px;
+        }
+
+        span.min-temperature {
+            margin-left: 15px;
+            color: #929292;
+        }
+
+        .time {
+            line-height: 25px;
+        }
+    </style>
+
+</head>
+<body>
+
+<div class="report-container">
+    <h2><?php echo $data->name; ?> Weather Status</h2>
+    <div class="time">
+        <div><?php echo date("l g:i a", $currentTime); ?></div>
+        <div><?php echo date("jS F, Y",$currentTime); ?></div>
+        <div><?php echo ucwords($data->weather[0]->description); ?></div>
+    </div>
+    <div class="weather-forecast">
+        <img
+            src="http://openweathermap.org/img/w/<?php echo $data->weather[0]->icon; ?>.png"
+            class="weather-icon" /> <?php echo $data->main->temp_max; ?>&deg;C<span
+            class="min-temperature"><?php echo $data->main->temp_min; ?>&deg;C</span>
+    </div>
+    <div class="time">
+        <div>Humidity: <?php echo $data->main->humidity; ?> %</div>
+        <div>Wind: <?php echo $data->wind->speed; ?> km/h</div>
+    </div>
+</div>
+
+
+</body>
+</html>
+
+<?php
+    }
+    elseif(isset($_POST['city'])) {
+        try {
+            $response = $weatherAPI->getWeatherReport($_POST['city']);
+            echo $response;
+        } catch (Exception $e) {
+            echo 'Message: ' .$e->getMessage();
+        }
+    }
+    else {
+        echo '<div class="report-container">';
+        echo '<h2>Please pass City or Country in URL</h2>';
+        echo '</div>';
+    }
+?>
